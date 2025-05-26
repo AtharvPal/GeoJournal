9871c6991103524b827f4daa23346f8f4f0484f4
Atharv <32848150+AtharvPal@users.noreply.github.com>
2025-05-26 15:41:27 +0530

    adding multer for file backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const { getUsers, signUp, login } = require("../controllers/users-controller");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get("/", getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   check("name").not().isEmpty(),
   check("email").normalizeEmail().isEmail(),
   check("password").isLength({ min: 6 }),
